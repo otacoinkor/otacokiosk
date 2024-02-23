bf77622e2f4da2a1a782dcdea640fa993e3df214
otacoinkor <down80kg@gmail.com>
2024-02-23 17:17:58 +0900

    test

--- main.py
+++ main.py
@@ -16,9 +16,11 @@ def split_frame(input_df, rows):
 st.set_page_config(layout="wide")
 
 with st.sidebar:
-    st.title('메뉴 1')
-    st.title('메뉴 2')
-    st.title('메뉴 3')
+    st.title('메뉴1')
+    st.header('서브1')
+    st.header('서브2')
+    st.title('메뉴2')
+    st.title('메뉴3')
 
 connection = pymysql.connect(
     host=config.host,
